4304a750c75ddfb2b8306e3cfeba7ed703813cc6
Fjeanneret <38987539+Fjeanneret@users.noreply.github.com>
2019-01-23 18:50:46 +0100

    Update Id_gene_fetch.py

--- Id_gene_fetch.py
+++ Id_gene_fetch.py
@@ -11,9 +11,6 @@ print(lignes)'''
 with open("GeneSymbols.txt") as f:
     lines = f.read().splitlines() 
 
-
-#voir pour asso nom du gne et espce sinon doublon 
-#un dictio avec cles / entrees pour a puis autres pour data ?
 for line in lines:
 	line = line.replace(" ", "_") #penser a gerer l'espace a la fin du premier Homo_sapiens
 	GeneSymbolAndSpecie =  line.split("\t")
@@ -44,27 +41,4 @@ while i < len(GeneSymbols):
 	ID_ENSG.append(decoded[0]["id"])
 	i+=1
 	
-	
 print(ID_ENSG)
-''' lien genome browser avec l'id
-www.ensembl.org/Homo_sapiens/Location/View?db=core;g=ENSG00000139618;
-
-
-lien prot avec l'ENST (prot summary mais pas protein sequence :/
-
-http://www.ensembl.org/Homo_sapiens/Transcript/ProteinSummary?
-db=core;g=ENSG00000139618;r=13:32315474-32400266;t=ENST00000380152
-
-orthologues :
-
-http://www.ensembl.org/Homo_sapiens/Gene/Compara_Ortholog?db=core;
-g=ENSG00000012048;r=17:43044295-43170245
-
-gerer si pas dans "core"
-
-http://plants.ensembl.org/Arabidopsis_thaliana/Gene/Compara_Ortholog?
-db=core;g=AT2G18790;r=2:8139756-8144461
-
-prendre type en amont ?
-
-'''
